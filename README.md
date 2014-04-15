b46f2080b68084d466da4429f32971c6dd86841b
xuzhenqi <xuzhenqi1993@gmail.com>
2014-04-15 14:07:30 +0800

    add svn

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ UbuntuFirstConfig
 =================
 
 After installing Ubuntu OS, the first thing to do is to run UbuntuFirstConfig.
+
+Software
+===
+
++chromium-browser
++flashplugin-installer
++subversion
++vim
++ibus
++multiget
++wine1.7
++openssh-server
+
+Instructions
+===
+
++Matlab
---
+++ Ubuntu_config.sh
@@ -0,0 +1,49 @@
+#########################################################################
+# File Name: Ubuntu_config.sh
+# Author: xuzhenqi
+# mail: xuzhenqi1993@gmail.com
+# Created Time: Tue 25 Feb 2014 03:46:51 PM CST
+#########################################################################
+#!/bin/bash
+
+# software source config
+echo 'software source config'
+cp /etc/apt/sources.list /etc/apt/sources.list.back
+cp config/sources.list /etc/apt/sources.list
+sudo apt-get update
+
+# software set up
+echo 'software set up'
+sudo apt-get install chromium-browser flashplugin-installer vim ibus ibus-clutter ibus-gtk ibus-gtk3 ibus-qt4 multiget subversion
+
+# input method config
+im-switch -s ibus
+ibus-setup
+
+# install wine
+sudo add-apt-repository ppa:ubuntu-wine/ppa
+sudo apt-get update
+sudo apt-get install wine1.7
+
+# firofox uninstall
+echo 'firefox uninstall'
+sudo apt-get remove firefox firefox*
+
+# vim config
+echo 'vim config'
+cp config/.vimrc ~/.vimrc
+
+# software upgrade
+sudo apt-get upgrade
+
+# github config
+
+# install Matlab
+
+# install Theano
+
+# install Pylearn2
+
+# install ssh-server
+sudo apt-get install openssh-server
+sudo service ssh start
